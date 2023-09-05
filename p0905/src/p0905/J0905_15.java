8311e1578f15151461d204360237cc2ec3e919de
KOREAVC <KOREAVC@DESKTOP-L3232R5>
2023-09-05 18:23:21 +0900

    랜덤 숫자 맞추기 개인연습

---
+++ p0905/src/p0905/J0905_15.java
@@ -0,0 +1,37 @@
+package p0905;
+
+import java.util.Scanner;
+
+public class J0905_15 {
+
+	public static void main(String[] args) {
+		// 1-100 랜덤숫자를 생성해서 맞추는 프로그램을 구현하시오.
+		Scanner scan = new Scanner(System.in);
+		int ran = (int)(Math.random()*100)+1;
+		int[] arr = new int[10];
+		int i=0;
+		//System.out.println(ran);
+		
+		while(i<10) {
+			System.out.println("숫자를 입력하시오.>>");
+			int input = scan.nextInt();
+			arr[i] = input;
+			System.out.printf("%d", arr[i]);
+			//System.out.println(ran);
+			if(ran==input) {
+				System.out.println("정답입니다.");
+				System.out.println("");
+				break;
+			}else if(ran>input) {
+				System.out.println("입력한 값보다 큰수입니다.");
+			}else {
+				System.out.println("입력한 값보다 작은수입니다.");
+			}
+			if(i==9) {
+				break;
+			}
+		}
+		
+	}//main
+
+}//class
